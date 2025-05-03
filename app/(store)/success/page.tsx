a8f305f3f0a8f03864297f45a801334691738919
arhamgill <arhamgillcoder@gmail.com>
2025-05-03 11:32:57 +0500

    added success page

--- app/(store)/success/page.tsx
+++ app/(store)/success/page.tsx
@@ -1,9 +1,23 @@
-import React from "react";
+"use client";
+import React, { useEffect } from "react";
+import { useBasketStore } from "@/store/store";
+import { useSearchParams } from "next/navigation";
 
 function page() {
+  const searchParams = useSearchParams();
+  const { clearBasket } = useBasketStore();
+  const orderNumber = searchParams.get("orderNumber");
+
+  useEffect(() => {
+    if (orderNumber) {
+      clearBasket();
+    }
+  }, [orderNumber, clearBasket]);
+
   return (
     <div className="text-center mt-8">
-      Payment Successful! Plz check the confirmation email sent to you.
+      Payment Successful for order number! Plz check the confirmation email sent
+      to you.
     </div>
   );
 }
