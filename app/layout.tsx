5842f7db32083ab4a8d33c4f23539b925c7a2802
arhamgill <arhamgillcoder@gmail.com>
2025-06-21 12:58:59 +0500

    logo and heading

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,9 @@ export const metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning={true}>
+      <head>
+        <link rel="icon" href="/website.ico" />
+      </head>
       <body>{children}</body>
     </html>
   );
